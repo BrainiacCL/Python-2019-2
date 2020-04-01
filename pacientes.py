bade1e34e5404bf0b04826f8a8b09dcf93204bbe
Brainiac <brayan.elgueta1501@alumnos.ubiobio.cl>
2020-04-01 02:55:56 -0300

    se agrego modulo de pacientes listar y insertar

--- pacientes.py
+++ pacientes.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, url_for, redirect
 from flaskext.mysql import MySQL
 import pymysql
 import pymysql.cursors
@@ -14,6 +14,22 @@ mysql.connect_args["autocommit"] = True
 mysql.connect_args["cursorclass"] = pymysql.cursors.DictCursor
 
 
+@app.route('/nuevoPaciente', methods=["POST"])
+def pacienteNuevo():
+	cursor = mysql.get_db().cursor()
+	try:
+
+		rut = request.form["rut"]
+		nombre = request.form["nombre"]
+		fecha_nacimiento = request.form["fecha_nacimiento"]
+		sql = "INSERT INTO paciente (rut, nombre, fecha_nacimiento) VALUES (%s,%s,%s)"
+		cursor.execute(sql,(rut, nombre, fecha_nacimiento))
+
+	except Exception as e:
+		print(e)	
+		
+	return redirect(url_for('pacientes'))
+
 @app.route('/pacientes')
 def pacientes():
 	cursor = mysql.get_db().cursor() #llamada a la base de datos	
@@ -23,6 +39,11 @@ def pacientes():
 
 	return render_template("pacientes.html", pacients = pacientes)
 
+
+@app.route('/vacunar')
+def vacunacion():
+	cursor = mysql.get_db().cursor()
+	cursor.execute
 
 if __name__ == "__main__":
 	app.run(debug=True)
